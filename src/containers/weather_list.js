47b4d4d2042c7802c25d5b2ba230ac87adef6886
dianacamacho <dcam1901@yahoo.com>
2017-04-09 01:57:22 -0500

    moved sparklines to own reusable chart component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Sparklines, SparklinesLine } from 'react-sparklines'
-
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const cityId = cityData.city.id;
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
    
     return (
       <tr key={cityId}>
         <td>{name}</td>
         <td>
-          <Sparklines height={120} width={180} data={temps}>
-            <SparklinesLine color="red" />
-          </Sparklines>
+          <Chart data={temps} color="blue" units="K"/>
+        </td>
+        <td>
+          <Chart data={humidities} color="orange" units="%"/>
+        </td>
+        <td>
+          <Chart data={pressures} color="green" units="hPa"/>
         </td>
       </tr>
     );
@@ -29,8 +35,8 @@ class WeatherList extends Component {
             <tr>
               <th>City</th>
               <th>Temperature</th>
-              <th>Pressure</th>
               <th>Humidity</th>
+              <th>Pressure</th>
             </tr>
           </thead>
           <tbody>
